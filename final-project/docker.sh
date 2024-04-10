88e1c2334b9c93e7fd08b4806d838e5c81cf8216
David <alwaysbrother10@gmail.com>
2024-04-10 07:06:43 -0400

    added detached docker scipt

--- final-project/docker.sh
+++ final-project/docker.sh
@@ -4,8 +4,8 @@ echo "Stopping all running containers..."
 sudo docker stop $(sudo docker ps -aq)
 
 #echo "Starting up services with docker-compose..."
-#docker-compose up
+#docker-compose up -d
 
 echo "Rebuilding services (if code was changed)..."
-docker-compose up --build
+docker-compose up -d --build
 
