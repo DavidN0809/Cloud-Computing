16f55d4b8677fc4c7790cb4245b7048c3de5be37
david <alwaysbrother10@gmail.com>
2024-04-27 03:05:14 +0000

    more cors stuff

--- final-project/src/api-gateway/api.go
+++ final-project/src/api-gateway/api.go
@@ -38,17 +38,27 @@ func main() {
 
 func corsMiddleware(h http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Access-Control-Allow-Origin", "*") // Allow any domain
-        w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE") // Allowed methods
+        // Specify the domain of your front-end app
+        origin := r.Header.Get("Origin")
+        allowedOrigins := map[string]bool{
+            "http://localhost:3000": true,
+        }
+
+        // Check if the origin is in the allowed list
+        if _, ok := allowedOrigins[origin]; ok {
+            w.Header().Set("Access-Control-Allow-Origin", origin)
+            w.Header().Set("Access-Control-Allow-Credentials", "true")
+        }
+
+        w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
         w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
-        // If it's a preflight OPTIONS request, send an OK status and return
         if r.Method == "OPTIONS" {
             w.WriteHeader(http.StatusOK)
             return
         }
 
-        h.ServeHTTP(w, r) // Serve the original request
+        h.ServeHTTP(w, r)
     })
 }
 
